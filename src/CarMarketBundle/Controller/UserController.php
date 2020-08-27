3ef12811fe14edeb990ed5cb01b4b49b23ad0e24
Marchinka90 <33801612+Marchinka90@users.noreply.github.com>
2020-08-27 13:20:58 +0300

    User registered successfuly

--- src/CarMarketBundle/Controller/UserController.php
+++ src/CarMarketBundle/Controller/UserController.php
@@ -2,9 +2,12 @@
 
 namespace CarMarketBundle\Controller;
 
-use CarMarketBundle\Entity\Car;
+use CarMarketBundle\Entity\Role;
+use CarMarketBundle\Entity\User;
+use CarMarketBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends Controller
@@ -17,5 +20,72 @@ class UserController extends Controller
     public function register(Request $request)
     {   
         return $this->render('users/register.html.twig', ['form' => $this->createForm(UserType::class)->createView()]);
+    }
+
+    /**
+     * @Route("register", methods={"POST"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function registerProcess(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        $email = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $form['email']->getData()]);
+
+        if ($email !== null) {
+            $email = $email->getEmail();
+            $this->addFlash("errors", "Email $email already taken");
+            return $this->returnRegisterView($user);
+        }
+
+        if($form['password']['first']->getData() !== $form['password']['second']->getData()){
+            $this->addFlash("errors", "Password mismatch");
+            return $this->returnRegisterView($user);
+        }
+
+        $passwordHash = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPassword());
+        $user->setPassword($passwordHash);
+
+        $isAdmin = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        if (!$isAdmin) {
+            $role = $this
+                ->getDoctrine()
+                ->getRepository(Role::class)
+                ->findOneBy(['name' => 'ROLE_ADMIN']);
+        } else {
+             $role = $this
+                ->getDoctrine()
+                ->getRepository(Role::class)
+                ->findOneBy(['name' => 'ROLE_ADMIN']);
+        }
+
+        var_dump($role);
+        exit;
+        
+        $user->addRole($role);
+        $user->setStatus(1);
+        
+        $em = $this->getDoctrine()->getManager();
+		$em->persist($user);
+		$em->flush();
+
+		$this->addFlash("success", "User was created successfuly");
+        return $this->redirectToRoute('user_register');
+        
+    }
+
+    /**
+     * @param User $user
+     * @return Response
+     */
+    private function returnRegisterView(User $user): Response
+    {
+        return $this->render('users/register.html.twig', ['user' => $user, 'form' => $this->createForm(UserType::class)->createView()]);
+
     }
 }
--- src/CarMarketBundle/Entity/User.php
+++ src/CarMarketBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace CarMarketBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * User
@@ -11,7 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="users")
  * @ORM\Entity(repositoryClass="CarMarketBundle\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @var int
@@ -236,6 +237,29 @@ class User
         }
 
         return $stringRoles;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+    
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
     }
 }
 
