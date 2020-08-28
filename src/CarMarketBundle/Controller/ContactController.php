24984e6943919a65d2e75f65cadd186ccf5d4be3
Marchinka90 <33801612+Marchinka90@users.noreply.github.com>
2020-08-28 11:00:24 +0300

    View create contact

--- src/CarMarketBundle/Controller/ContactController.php
+++ src/CarMarketBundle/Controller/ContactController.php
@@ -2,12 +2,44 @@
 
 namespace CarMarketBundle\Controller;
 
-use CarMarketBundle\Entity\Car;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use CarMarketBundle\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends Controller
 {
-	
+	/**
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     * @Route("contact", name="create_contact", method={"GET"})
+     */
+    public function create()
+    {
+        $contact = $this->getDoctrine()->getRepository(Contact::class)->findBy(['user' => $this->getUser()]);
+        return $this->render('users/contact/create.html.twig', ['contact' => $contact, 'form' => $this->createForm(ContactType::class)->createView()]);
+    }
+
+    /**
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     * @Route("contact", method={"POST"})
+     */
+    public function createProcess(Request $request)
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($contact);
+        if (count($errors) > 0) {
+            foreach ($errors as $error => $value) {
+                $this->addFlash("errors", $value->getMessage()); 
+            }
+            return $this->render('users/contact/create.html.twig', ['contact' => $contact, 'form' => $this->createForm(ContactType::class)->createView()]);
+        }
+
+        var_dump('tuk');
+        exit;
+    }
 }
