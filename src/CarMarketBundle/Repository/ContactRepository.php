ade791ad9cdbbf0d37f72eccc11235349a3e37b9
Marchinka90 <33801612+Marchinka90@users.noreply.github.com>
2020-08-29 09:00:11 +0300

    insert and update methods in cantact repository

--- src/CarMarketBundle/Repository/ContactRepository.php
+++ src/CarMarketBundle/Repository/ContactRepository.php
@@ -2,6 +2,10 @@
 
 namespace CarMarketBundle\Repository;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping;
+use SoftUniBlogBundle\Entity\Contact;
+
 /**
  * ContactRepository
  *
@@ -10,4 +14,39 @@ namespace CarMarketBundle\Repository;
  */
 class ContactRepository extends \Doctrine\ORM\EntityRepository
 {
+	public function __construct(EntityManagerInterface $em, Mapping\ClassMetadata $metaData = null)
+	{
+		parent::__construct($em, $metaData ==  null ? new Mapping\ClassMetadata(Contact::class) : $metaData);
+	}
+
+	/**
+	 * @param Contact $contact 
+	 * @return bool
+	 */
+	public function insert(Contact $contact)
+	{
+		try {
+			$this->_em->persist($contact);
+			$this->_em->flush();
+			return true;
+		} catch (OptimisticLockException $e) {
+			return false;
+		}
+	}
+
+	/**
+	 * @param Contact $contact 
+	 * @return bool
+	 */
+	public function update(Contact $contact)
+	{
+		try {
+			$this->_em->merge($contact);
+			$this->_em->flush();
+			return true;
+		} catch (OptimisticLockException $e) {
+			return false;
+		}
+	}
+
 }
